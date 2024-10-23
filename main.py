0a064901ad8841576b3bfa930107db71ef322a38
Olga Kaufman <ok025879@tite.amiedu-ict.net>
2024-10-23 12:52:29 +0300

    okokok-OOO-okok

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ def main()
 # git commit -m "okokokokok"
 # git push origin master
 
+# joku muutos 
+
+# git add . 
+
+# git commit -m "okokokokok" 
+
+# git push origin master 
+
+# git pull origin master  
+
+# git add . 
+
+# git stash 
+
+# git stash pop 
+
+# git status 
+
+# git push origin master 
+
+ 
+ 
